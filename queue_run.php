6a432a0ec0b86fa307bbce459835380bf3057484
Arseno25 <utomnestagumara@gmail.com>
2024-11-22 10:01:27 +0700

    :sparkles: feat(queue_run): Update queue_run.php to use absolute path for artisan command

--- queue_run.php
+++ queue_run.php
@@ -1,9 +1,15 @@
 <?php
 
-exec('php artisan queue:work --stop-when-empty', $output, $status);
+$artisanPath = __DIR__ . '/artisan';
+
+if (!file_exists($artisanPath)) {
+    die("File 'artisan' tidak ditemukan di path: $artisanPath\n");
+}
+
+exec("php $artisanPath queue:work --stop-when-empty", $output, $status);
 
 if ($status === 0) {
-    echo "Command berhasil dijalankan:\n";
+    echo "Command 'php artisan queue:work --stop-when-empty' berhasil dijalankan di background.\n";
 } else {
     echo "Command gagal dijalankan dengan status: $status\n";
     print_r($output);
